5b9e208cfe80a44ac1dd868e73dbfaef4e5cb38d
ReltonTolpo <ori.taylor@gmail.com>
2016-02-13 22:01:38 +0000

    Fixing Base Code
    Updating code to latest version of Lua Love

--- conf.lua
+++ conf.lua
@@ -1,4 +1,7 @@
 function love.conf(t)
-	t.title = "Platformer"
-	t.screen.width = 1200
+
+	t.title = "CompetitionGame"
+	t.window.width = 1200
+	t.window.height = 750
+	
 end
--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 function love.load()
 
+	love.graphics.setBackgroundColor(255, 255, 255)
+
 end
 
 function love.update(dt)
 
+	
+
 end
 
 function love.draw()
+
+	
 
 end
