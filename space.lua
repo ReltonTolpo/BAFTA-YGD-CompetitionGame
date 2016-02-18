f524abab4a58c394e0263b588d289d4d840e6dbf
ReltonTolpo <ori.taylor@gmail.com>
2016-02-18 10:22:24 +0000

    Doing more star stuff
    Its kind of working now, but not really

--- space.lua
+++ space.lua
@@ -2,10 +2,7 @@ space = {}
 
 function space.load()
 
-	space.i = 1
-	space.maxStars = 100
-	space.minStars = 30
-	space.starNum = love.math.random(space.minStars, space.maxStars) 
+	space.starNum = love.math.random(30, 100) 
 	space.starSize = 1
 	space.starXPosition = 1
 	space.starYPosition = 1
@@ -17,12 +14,11 @@ function space.draw()
 	love.graphics.setBackgroundColor(0, 0, 0)
 	love.graphics.setColor(255, 255, 255)
 
-	while space.i < space.starNum do
+	for i = 1, space.starNum do
 		space.starSize = love.math.random(1, 10)
 		space.starXPosition = love.math.random(1, 1200)
 		space.starYPosition = love.math.random(1, 750)
 		love.graphics.rectangle("fill", space.starXPosition, space.starYPosition, space.starSize, space.starSize)
-		space.i = space.i + 1
 	end
 end
 
