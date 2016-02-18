211f0fc8d46ae9aa0f3f25dde7c53d3af351cb3b
ReltonTolpo <ori.taylor@gmail.com>
2016-02-18 10:53:20 +0000

    Try to draw stars once
    Please just work ;-( I NEED WORKING STARS

--- space.lua
+++ space.lua
@@ -2,24 +2,39 @@ space = {}
 
 function space.load()
 
-	space.starNum = love.math.random(30, 100) 
-	space.starSize = 1
-	space.starXPosition = 1
-	space.starYPosition = 1
+	space.starNum = love.math.random(20, 100) 
+	space.starSize = 10
+	space.starXPosition = 100
+	space.starYPosition = 100
 
 end
 
 function space.draw()
 
 	love.graphics.setBackgroundColor(0, 0, 0)
-	love.graphics.setColor(255, 255, 255)
+
+end
+
+function space.drawStars()
 
 	for i = 1, space.starNum do
+		love.graphics.setColor(255, 255, 255)
 		space.starSize = love.math.random(1, 10)
 		space.starXPosition = love.math.random(1, 1200)
 		space.starYPosition = love.math.random(1, 750)
 		love.graphics.rectangle("fill", space.starXPosition, space.starYPosition, space.starSize, space.starSize)
 	end
+
+end
+
+function only_run_once(func)
+    return function (...)
+        if func then
+            local f = func
+            func = nil
+            return f(...)
+        end
+    end
 end
 
 function UPDATE_SPACE(dt)
@@ -29,5 +44,6 @@ end
 function DRAW_SPACE()
 
 	space.draw()
+	space.drawStars()
 
 end
