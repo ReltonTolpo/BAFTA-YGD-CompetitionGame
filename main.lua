21129eb4a4e42e316bb3e617292edbc6f98a3824
danny2303 <danzo2303@gmail.com>
2016-02-27 15:00:54 +0000

    Update main.lua
    Fixed the planets - 'C' no longer crashes but also doesn't work!

--- main.lua
+++ main.lua
@@ -10,32 +10,36 @@ function love.load()
 
 	--Loading Classes
 	space.load()
-	player.load()
-
 	planetA.load()
 	planetB.load()
 	planetC.load()
+
+	player.load()
+
+
 	
 end
 
 function love.update(dt)
 
 	UPDATE_SPACE(dt)
-	UPDATE_PLAYER(dt)
-
-	UPDATE_PLANETA(dt)
+		UPDATE_PLANETA(dt)
 	UPDATE_PLANETB(dt)
 	UPDATE_PLANETC(dt)
+
+	UPDATE_PLAYER(dt)
+
 
 end
 
 function love.draw()
 
 	DRAW_SPACE()
-	DRAW_PLAYER()
-
-	DRAW_PLANETA()
+		DRAW_PLANETA()
 	DRAW_PLANETB()
 	DRAW_PLANETC()
+	DRAW_PLAYER()
+
+
 
 end
