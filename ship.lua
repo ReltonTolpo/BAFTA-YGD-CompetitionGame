5ecebf4ff1b23bdfaf3b98dd5f24a4d4b9626332
jon2000 <jonmichaelharris@gmail.com>
2016-03-10 20:59:03 +0000

    (Jon) Hopefully fixed merge issue

--- ship.lua
+++ ship.lua
@@ -13,7 +13,6 @@ function ship.load()
 end	
 
 function update(dt)
---<<<<<<< HEAD
 	n = 0
 if love.keyboard.isDown('e') and player.x < 300 and player.x > 100 and shipActive == false then
 
@@ -26,28 +25,13 @@ if love.keyboard.isDown('e') and player.x < 300 and player.x > 100 and shipActiv
 elseif love.keyboard.isDown('e') and shipActive == true then
 	ship = love.graphics.newImage("images/ship/shipLand.png")
 
-<<<<<<< HEAD
-	if player.onPlanet == true then
-		if love.keyboard.isDown('e') and player.x < 400 and player.x > 100 and player.y > 400 and shipActive == false then
-			ship = love.graphics.newImage("images/ship/shipLandPlayer.png")
-			player.playerExists = false
-			shipActive = true
-			love.timer.sleep(0.5)
-		elseif love.keyboard.isDown('e') and shipActive == true then
-			ship = love.graphics.newImage("images/ship/shipLand.png")
-			player.x = 100
-			player.playerExists = true
-			shipActive = false
-			love.timer.sleep(0.1)
-		end
-=======
+
 		player.x = 100
 		player.playerExists = true
 		player.doGravity = true
 		shipActive = false
 		    playerOverShip = false
 		love.timer.sleep(0.1)
->>>>>>> origin/master
 	end
 
 end
@@ -55,7 +39,6 @@ end
 function draw()
 
 	love.graphics.setColor(255, 255, 255)
---<<<<<<< HEAD
 	love.graphics.draw(ship, 50 , 350, 0, 7, 7)
 if shipActive == true then
 
