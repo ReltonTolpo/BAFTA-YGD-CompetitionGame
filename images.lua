6d909247cfc8393c818468d45106f8ea2e40b79b
ReltonTolpo <ori.taylor@gmail.com>
2016-03-12 15:53:30 +0000

    Fixing Benjamin's double e
    -Double E

--- images.lua
+++ images.lua
@@ -7,7 +7,7 @@ function images.load()
 	images.playerDead 	= love.graphics.newImage("images/player/playerDead.png") 		-- Dead player
 	images.playerSpace 	= love.graphics.newImage("images/player/playerIdleSpace.png") 	-- Player in space
 	images.playerLeft 	= love.graphics.newImage("images/player/playerWalkingLeft.png")	-- Player moving left
-	images.playerRight 	= love.graphics.newImage("images/player/playerWalkingRight.png")--Playeer moving right
+	images.playerRight 	= love.graphics.newImage("images/player/playerWalkingRight.png") --Player moving right
 
 	--↓ Ship images
 	images.ship 		= love.graphics.newImage("images/ship/shipLand.png")			--Ship without player inside
