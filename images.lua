43b1536004cf6ca7f23ac8879ea572cdc9fde9ed
bbroadbent <benjamin.d.broadbent@gmail.com>
2016-03-12 10:58:34 +0000

    Adding tabs
    +More organized code
    +More comments
    -may hurt Ori's OCD

--- images.lua
+++ images.lua
@@ -3,22 +3,22 @@ images = {}
 function images.load()
 
 	--↓ Player images 
-	images.playerIdle = love.graphics.newImage("images/player/playerIdle.png") -- Still player
-	images.playerDead = love.graphics.newImage("images/player/playerDead.png") -- Dead player
-	images.playerSpace = love.graphics.newImage("images/player/playerIdleSpace.png") -- Player in space
-	images.playerLeft = love.graphics.newImage("images/player/playerWalkingLeft.png")	-- Player moving left
-	images.playerRight = love.graphics.newImage("images/player/playerWalkingRight.png") --Playeer moving right
+	images.playerIdle 	= love.graphics.newImage("images/player/playerIdle.png") 		-- Still player
+	images.playerDead 	= love.graphics.newImage("images/player/playerDead.png") 		-- Dead player
+	images.playerSpace 	= love.graphics.newImage("images/player/playerIdleSpace.png") 	-- Player in space
+	images.playerLeft 	= love.graphics.newImage("images/player/playerWalkingLeft.png")	-- Player moving left
+	images.playerRight 	= love.graphics.newImage("images/player/playerWalkingRight.png")--Playeer moving right
 
 	--↓ Ship images
-	images.ship = love.graphics.newImage("images/ship/shipLand.png")
-	images.playerInShip = love.graphics.newImage("images/ship/shipLandPlayer.png")
+	images.ship 		= love.graphics.newImage("images/ship/shipLand.png")			--Ship without player inside
+	images.playerInShip	= love.graphics.newImage("images/ship/shipLandPlayer.png")		--Ship with player inside
 
 	--↓ Monster images
-	images.darkElf = love.graphics.newImage("images/monster/darkElf.png")
+	images.darkElf 		= love.graphics.newImage("images/monster/darkElf.png")			--Dark elf monster
 
 	--↓ Weather images
-	images.moon = love.graphics.newImage("images/weather/moon.png")
-	images.sun = love.graphics.newImage("images/weather/sun.png")
+	images.moon 		= love.graphics.newImage("images/weather/moon.png")				--Moon
+	images.sun 			= love.graphics.newImage("images/weather/sun.png")				--Sun
 
 end
 
