70610dc696bec134a4ea3857f4f17e4ce24aacde
Benjamin Broadbent <benjamin.d.broadbent@gmail.com>
2016-04-29 13:27:13 +0100

    Implemented space music to player.lua
    Not tested due to working on this at school

--- player.lua
+++ player.lua
@@ -201,7 +201,14 @@ function player.update(dt)
 
 	player.currentGravity = planetArray[currentPlanet][4]
 	player.weight = player.currentGravity * player.mass
-
+	
+	if player.onPlanet = true then
+		sound.bg_music:play()
+		sound.bg_music_space:pause()
+	elseif player.onPlanet = false then
+		sound.bg_music_space:play()
+		sound.bg_music:pause()		
+	end
 end
 
 function player.boundary()
