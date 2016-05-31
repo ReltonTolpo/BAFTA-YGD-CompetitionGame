2e036f4af571e5c653596a9f825284a5e5191baa
ReltonTolpo <ori.taylor@gmail.com>
2016-05-31 19:11:46 +0100

    Gary is talkin' nows

--- endgame.lua
+++ endgame.lua
@@ -15,7 +15,7 @@ function endgame.load()
     garyx = 800
     garyy = 450
     endgame.endgame = false
-    counter = 0
+    endgame.counter = 0
     hintString = "Welcome home\n adventurer!"
 
   local fireworks = images.firework
@@ -60,47 +60,50 @@ end
 
 function endgame.animation()
 
+    function garytag(x,y,text,a)
+
+        if endgame.endgame == true then
+            love.graphics.setColor(207, 190, 4, a)
+            love.graphics.rectangle("fill", x-200, y-200, 200, 120 )
+            love.graphics.line(x-200, y-200, x, y)
+            love.graphics.setColor(0, 0, 0, a)
+            love.graphics.print(text, x-200, y-200, 0, 1, 1)
+        end
+
+    end
+
     if endgame.endgame == true then
 
         love.graphics.setColor(255,255,255,255)
         love.graphics.draw(images.gary, garyx, garyy, 0, 2, 2)
+
+        if endgame.counter < 500 then
+            hintString = "Welcome to Earth!"
+        end
+        if endgame.counter > 500 and endgame.counter < 1000 then
+            hintString = "Please deposit \n your precious\n GRAVITONS to \n restore the \n planet's gravity!"
+        end
+
+        endgame.counter = endgame.counter + 1
 
     end
 
 end
 
 function UPDATE_ENDGAME(dt)
+
     psystem81:update(dt)
-    psystem81:setColors(love.math.random(-255, 255), love.math.random(-255, 255), love.math.random(-255, 255), 255)-- Fade to transparency.
+    psystem81:setColors(love.math.random(-255, 255), love.math.random(-255, 255), love.math.random(-255, 255), 255)-- Fade to transparency
+
 end
 
 function DRAW_ENDGAME()
 
-
 	endgame.wormhole()
     endgame.animation()
-    if endgame.endgame == true then love.graphics.draw(psystem81,garyx + 70 ,garyy)
 
-        if player.counter < 500 then
-            hintString = "Welcome to Graviton Galaxy!"
-        end
-                if player.counter > 500  and player.counter < 1000 then
-            hintString = "Please deposit \n your precious\n GRAVITONS to \n restore the \n planet's gravity!"
-        end
-
-
-        function garytag(x,y,text,a)
-
-        love.graphics.setColor(207, 190, 4, a)
-        love.graphics.rectangle("fill", x-200, y-200, 200, 120 )
-        love.graphics.line(x-200, y-200, x, y)
-        love.graphics.setColor(0, 0, 0, a)
-        love.graphics.print(text, x-200, y-200, 0, 1, 1)
-
-    end
-    counter = counter + 1
-     garytag(garyx, garyy, hintString, player.alpha)
- end
-
+    if endgame.endgame == true then love.graphics.draw(psystem81,garyx + 70, garyy) end
+    
+    garytag(garyx, garyy, hintString, player.alpha)
 
 end
